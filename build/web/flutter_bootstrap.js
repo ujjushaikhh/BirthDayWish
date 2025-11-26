b6448a1fd43ebf53de63e3077cc5291535b699ee
ujefshaikh <snmtechno0@gmail.com>
2025-11-26 12:45:02 +0530

    Fix GitHub Pages base href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2634047975"
+    serviceWorkerVersion: "95897915"
   }
 });
